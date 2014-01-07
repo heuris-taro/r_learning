fdd8b4ce344b5516edb610a5752d39c6b98ad9c3
ken <ken@example.com>
2014-01-07 18:22:32 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RLearning::Application.config.secret_token = '10fb82272bb383eaedb5ac48c8d8b963999d47ead08e6aa5937d31a07464085df7a825adfbb0eab4227e699ceeb64277f56b25dbf0cb435d27d2e612e705ae88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RLearning::Application.config.session_store :cookie_store, key: '_r_learning_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RLearning::Application.config.session_store :active_record_store

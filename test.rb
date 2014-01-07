07ceb6aa820e8af0141a0780ac33339c20a43f5a
ken <ken@example.com>
2014-01-08 01:07:53 +0900

    Make a interaction of presentation app

--- test.rb
+++ test.rb
@@ -1,17 +1,21 @@
 #!ruby
 # encoding: UTF-8
 require './config/environment'
+require 'set'
+DEBUG = false
 
-tables = [Question, Choice, CorrectAnswer]
-tables.each do |t|
-  puts t
-  cs = t.method(:column_names)[]
-  cs.each do |c|
-    unless c == "created_at" || c == "updated_at"
-      print "#{c} "
+if DEBUG
+  tables = [Question, Choice, CorrectAnswer]
+  tables.each do |t|
+    puts t
+    cs = t.method(:column_names)[]
+    cs.each do |c|
+      unless c == "created_at" || c == "updated_at"
+	print "#{c} "
+      end
     end
+    puts
   end
-  puts
 end
 
 q = Question.first
@@ -22,8 +26,25 @@ q.choices.each do |c|
   puts
 end
 
-puts '正解は...'
-print "\n" * 25
-q.correct_answers.each {|ca| print"#{ca.choice_number} "}
-puts
+as = []
+loop do
+  cardinality = q.choices.size
+  print "1 - #{cardinality}:"
+  a = gets.to_i
+  break unless (1 .. cardinality) === a
+  as << a
+end
+
+correct_nums = q.correct_answers.map(&:choice_number)
+p as, correct_nums if DEBUG
+
+as = Set.new(as)
+puts "あなたは#{as.to_a}を選んだ"
+if as == Set.new(correct_nums)
+  print "\n正解\n\n"
+else
+  puts '間違い、正しくは:'
+  p correct_nums
+  puts
+end
 print q.explanation

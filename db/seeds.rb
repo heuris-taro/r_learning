ef5a5c9727fada2d291ca655df0adaeac54079d1
ken <ken@example.com>
2014-01-12 14:12:12 +0900

    append seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,60 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Question
 Question.delete_all
-question1 = Question.create(
+Choice.delete_all
+CorrectAnswer.delete_all
+
+###############################################################################
+##				Question n				     #
+###############################################################################
+## Question
+#question = Question.create(
+#  description: <<EODSC,
+#EODSC
+#  explanation: <<EOEPN
+#EOEPN
+#)
+#
+## Choice
+#Choice.create(
+# question_id: question.id,
+# choice_number: 1,
+# description: <<EODSC
+#EODSC
+#)
+#Choice.create(
+# question_id: question.id,
+# choice_number: 2,
+# description: <<EODSC
+#EODSC
+#)
+#Choice.create(
+# question_id: question.id,
+# choice_number: 3,
+# description: <<EODSC
+#EODSC
+#)
+#Choice.create(
+# question_id: question.id,
+# choice_number: 4,
+# description: <<EODSC
+#EODSC
+#)
+#
+## CorrectAnswer
+#CorrectAnswer.create(
+#  question_id: question.id,
+#  choice_number:
+#)
+#
+###############################################################################
+
+###############################################################################
+##				Question 1				     #
+###############################################################################
+# Question
+question = Question.create(
   description: <<EODSC,
 以下に示すRubyプログラムのうち、実際に実行してもエラーにならないものを1つ選んでください。
 EODSC
@@ -34,9 +85,8 @@ EOEPN
 )
 
 # Choice
-Choice.delete_all
 Choice.create(
- question_id: question1.id,
+ question_id: question.id,
  choice_number: 1,
  description: <<EODSC
 def foo(x)
@@ -46,7 +96,7 @@ foo()
 EODSC
 )
 Choice.create(
- question_id: question1.id,
+ question_id: question.id,
  choice_number: 2,
  description: <<EODSC
 def bar
@@ -57,7 +107,7 @@ bar(n)
 EODSC
 )
 Choice.create(
- question_id: question1.id,
+ question_id: question.id,
  choice_number: 3,
  description: <<EODSC
 n = 10
@@ -67,7 +117,7 @@ end
 EODSC
 )
 Choice.create(
- question_id: question1.id,
+ question_id: question.id,
  choice_number: 4,
  description: <<EODSC
 10.times do |i|
@@ -79,22 +129,74 @@ EODSC
 )
 
 # CorrectAnswer
-CorrectAnswer.delete_all
 CorrectAnswer.create(
-  question_id: question1.id,
+  question_id: question.id,
   choice_number: 3
 )
 
+##############################################################################
 
 
+##############################################################################
+#				Question 2				     #
+##############################################################################
+# Question
+question = Question.create(
+  description: <<EODSC,
+以下の選択肢のうち、エラーにならずに正常に実行が完了するコードを1つ選択してください。
+EODSC
+  explanation: <<EOEPN
+正解は(b)です。
 
+整数オブジェクトにおける「*」メソッドは、レシーバの整数に引数の数値を掛け算した結果を返します。
+この場合、「0xFace」は正当な16進数表記の整数オブジェクトを表しますので、問題無く掛け算は実行されます。
 
+(a)のコードは、範囲オブジェクト「1..10」を文字列化しようとしたものですが、「ArgumentErrot: bad value for range」の例外が発生します。
 
+範囲オブジェクトにメソッドを適用する場合は、カッコを用いて範囲オブジェクトのリテラルを明確にしなければなりません。
+従ってこの場合は「(1..10).to_s」としないと目的の動作をしてくれないことになります。
 
+(c)のコードは、整数オブジェクトに「+」メソッドを適用していますが、引数として文字列が与えられています。
+整数オブジェクトの「+」メソッドは、引数に文字列を取ることができないため、このコードは例外が発生します。
 
+(d)のコードは、Time#strftimeメソッドの引数に数値オブジェクトを3つ指定していますが、同メソッドの引数はフォーマット文字列を表す文字列オブジェクトのみを受け取るため、数値オブジェクトを引数に取ることはできず、例外が発生します。
+EOEPN
+)
 
+# Choice
+Choice.create(
+ question_id: question.id,
+ choice_number: 1,
+ description: <<EODSC
+(a) 1..10.to_s
+EODSC
+)
+Choice.create(
+ question_id: question.id,
+ choice_number: 2,
+ description: <<EODSC
+(b) 10.*(0xFace)
+EODSC
+)
+Choice.create(
+ question_id: question.id,
+ choice_number: 3,
+ description: <<EODSC
+(c) 10.+(“10”)
+EODSC
+)
+Choice.create(
+ question_id: question.id,
+ choice_number: 4,
+ description: <<EODSC
+(d) Time.now.strftime(1999, 12, 11)
+EODSC
+)
 
+# CorrectAnswer
+CorrectAnswer.create(
+  question_id: question.id,
+  choice_number:2
+)
 
-
-
-
+##############################################################################

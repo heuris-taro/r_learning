fb453fd83e1f2d02c2ecf04cc73498da652b98ad
Ken Sonoda <test@example.com>
2014-01-14 15:04:46 +0900

    Make a unit test for question

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,4 +1,8 @@
 class Choice < ActiveRecord::Base
   attr_accessible :choice_number, :description, :question_id
   belongs_to :question
+
+  validates :question_id, presence: true
+  validates :choice_number, :description, presence: true
+  validates :choice_number, numericality: {only_integer: true, greater_than: 0}
 end

0bb1c08a5a6f5c7c65a68d0f9b2cb6e6a9d2cd04
Ken Sonoda <test@example.com>
2014-01-14 15:04:46 +0900

    Make a validation for question

--- app/models/correct_answer.rb
+++ app/models/correct_answer.rb
@@ -1,4 +1,9 @@
+# encoding: utf-8
 class CorrectAnswer < ActiveRecord::Base
   attr_accessible :choice_number, :question_id
   belongs_to :question
+  
+  validates :question_id, presence: true
+  validates :choice_number, presence: true
+#  validates :choice_number # (1 .. ca.question.choices.size)に含まれるか
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
   attr_accessible :description, :explanation
   has_many :correct_answers, dependent: :destroy
   has_many :choices, dependent: :destroy
+
+  validates :description, :explanation, presence: true
 end

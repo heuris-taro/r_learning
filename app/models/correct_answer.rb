66af9a057fa0dafc73b1410695f712a29337dc08
ken <ken@example.com>
2014-01-18 15:20:37 +0900

    tweak validation of CorrectAnswer
    名前空間を清潔にした。Choiceに対応する一意性制約を追加。

--- app/models/correct_answer.rb
+++ app/models/correct_answer.rb
@@ -6,11 +6,10 @@ class CorrectAnswer < ActiveRecord::Base
   validates :question_id, presence: true
   validates :choice_number, presence: true
   validates :choice_number, numericality: {only_integer: true, greater_than: 0}
-
-  validate :must_be_member
-  def must_be_member
-    unless (1 .. self.question.choices.size) === self.choice_number
-      errors.add(:base, "must be member of choices")
+  validates :choice_number, uniqueness: {scope: :question_id}
+  validates_each :choice_number do |record, attr, value|
+    unless (1 .. record.question.choices.size) === value
+      record.errors.add(attr, "must be member of choices")
     end
   end
 end

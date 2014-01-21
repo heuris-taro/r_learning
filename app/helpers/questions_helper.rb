b16471f4dbec5bc91307b2120f864c7b107815a1
Ken Sonoda <test@example.com>
2014-01-21 16:35:43 +0900

    fix question view

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def num2alph(num, first_chr = ?a)
+    base = first_chr.ord.pred
+    (base + num).chr(__ENCODING__)
+  end
+  def format_choice(str, first_chr = ?a)
+    str.gsub(/%([1-9])/){num2alph ($1).to_i, first_chr}
+  end
 end

9b0e5a6d356d719cab40cb2ba0875757b6bf92d1
Ken Sonoda <test@example.com>
2014-01-22 15:26:59 +0900

    部分テンプレート化
    単純な選択肢と問題の表示を再利用する

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,20 +1,24 @@
 #coding: UTF-8
 # AdminControllerコントローラクラス
-# Author:: ANDO
-# Date:: 2014.01.16
 
 # 管理者のコントローラクラス
 class AdminController < ApplicationController
 
   
   def index
+    @user = User.find(session[:user_id])
+    #p 'DEBUG'
+    
 #    @total_orders = Order.count
     # 現在の登録問題数を取得する
+    # 名前を表示する
     @total_questions = Question.count
-
     @questions = Question.all
     
     # 問題管理画面へリダイレクトする
-    redirect_to controller: 'questions', action: 'index'
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @questions }
+    end
   end
 end
